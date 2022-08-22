844e28d6939b25bbc2f8b16c81abd6e3b4619e43
Sreya Nandanampati <111778548+sreyanan03@users.noreply.github.com>
2022-08-22 17:12:51 -0400

    Create SpellCheckerTester.java

---
+++ SpellCheckerTester.java
@@ -0,0 +1,10 @@
+public class SpellCheckerTester {
+
+    public static void main (String [] args) {
+        try{
+            SpellChecker spellCheck = new SpellChecker("words.txt");
+
+        } catch(Exception e) {}
+      
+    }
+}
